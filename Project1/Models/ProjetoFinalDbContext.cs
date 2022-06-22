c90a708e54398b81f05cc181cab36c7d453ebaa2
cleasholdorf <you@example.com>
2022-06-22 13:30:48 -0300

    first commit

---
+++ Project1/Models/ProjetoFinalDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class ProjetoFinalDbContext : DbContext
+    {
+        public ProjetoFinalDbContext(DbContextOptions<ProjetoFinalDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Project1/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+        public string nome { get; set; }
+        public List<Calculo>ListaDeCalculo {get;set;}
+    }
+}
